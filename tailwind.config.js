d76a4f3e5886b7742bf552622eb60fe7582ac0e3
hkldtrinanda <hkldtrinanda12@gmail.com>
2021-10-13 02:19:37 +0700

    test website

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: ["**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        Vintage: {
+          default: "#CEE5D0",
+          1: "#F3F0D7",
+          2: "#D8B384",
+          3: "#5E454B",
+          4: "#D9CDBA",
+          5: "#F0E3D0",
+          6: "#A28F71",
+          7: "#716358",
+          8: "#D3BD9A",
+          9: "#000000",
+          10: "#000000",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
